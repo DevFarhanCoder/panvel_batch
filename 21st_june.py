575b96bb657960253e5e079650ff304ad86b9bd0
farhangori89@gmail.com <farhangori89@gmail.com>
2025-06-21 17:32:06 +0530

    btwn commit

---
+++ 21st_june.py
@@ -0,0 +1,13 @@
+def sum(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+print(sum(10,20))
+print(sub(10,20))
+print(mul(10,20))
+print(div(10,20))
